b9b29a108d8072a65e88ff50848ece1feed5d071
pashkjel <json.jaden@gmail.com>
2024-07-01 10:10:06 -0700

    first

---
+++ src/routes/films/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+    let films = [
+        {id: 1, name: 'The Godfather', director: 'Francis Capolla'},
+        {id: 1, name: 'The Godfather', director: 'Francis Capolla'}
+    ]
+</script>
+
+<ul>
+    {#each films as film}
+    <li>
+        <a href="films/{film.id}">{film.name}</a>
+    </li>
+    {/each}
+</ul>
